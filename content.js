b681212ae4dbad2e1751cbf25b2e1503a44f5e5a
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2024-03-18 14:39:12 +0200

    Update content.js

--- content.js
+++ content.js
@@ -1,3 +1,5 @@
+console.log("Content script loaded");
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   return false;
 });
@@ -25,3 +27,45 @@ chrome.runtime.sendMessage({
   html: getHTML(),
   url: getURL(),
 });
+
+// listen for click on button with id "alert-offensive-btn" then run document.getElementById('alert-offensive')?.remove()
+
+/**
+ * Wait for an element before resolving a promise
+ * @param {String} querySelector - Selector of element to wait for
+ * @param {Integer} timeout - Milliseconds to wait before timing out, or 0 for no timeout
+ */
+function waitForElement(querySelector, timeout) {
+  return new Promise((resolve, reject) => {
+    var timer = false;
+    if (document.querySelectorAll(querySelector).length) return resolve();
+    const observer = new MutationObserver(() => {
+      if (document.querySelectorAll(querySelector).length) {
+        observer.disconnect();
+        if (timer !== false) clearTimeout(timer);
+        return resolve();
+      }
+    });
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+    if (timeout)
+      timer = setTimeout(() => {
+        observer.disconnect();
+        reject();
+      }, timeout);
+  });
+}
+
+waitForElement("#alert-offensive", 10000)
+  .then(function () {
+    document
+      .getElementById("alert-offensive-btn")
+      .addEventListener("click", () => {
+        document.getElementById("alert-offensive")?.remove();
+      });
+  })
+  .catch(() => {
+    console.log("Timed out waiting for element");
+  });
