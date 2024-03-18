e0727dc01f722119c51d42af00380e84e78b7c11
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2024-03-18 09:35:12 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("Extension installed!");
+});
+
 chrome.contextMenus.create({
   id: "check-cyberbully",
   title: "Detect Cyberbullying",
@@ -5,47 +9,119 @@ chrome.contextMenus.create({
 });
 
 chrome.contextMenus.onClicked.addListener(async function (info, tab) {
-  console.log(info);
   if (info.menuItemId === "check-cyberbully") {
-    console.log(info.selectionText);
     const formData = new FormData();
     formData.append("user_input", info.selectionText);
 
-    const response = await fetch("https://detection-main-production.up.railway.app/detect", {
-      method: "POST",
-      body: formData,
-    });
+    const response = await fetch(
+      "https://detection-main-production.up.railway.app/detect",
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
     const data = await response.json();
-    console.log(data?.details);
     const isOffensive = data?.details?.offensive;
     const reasons = data?.details?.offensive_reasons;
     const multiClass = data?.details?.multi_class_result;
-    // chrome.tabs.sendMessage(tab.id, { result: data });
 
     // open pop up
     chrome.windows.create({
       url: "popup.html",
       type: "popup",
-      width: 500,
+      width: 700,
       height: 800,
       top: 100,
       left: 100,
     });
 
     setTimeout(async () => {
-      // send the data to the popup via content script message
       const [tab] = await chrome.tabs.query({
         active: true,
         lastFocusedWindow: true,
       });
-      console.log({tab});
+      console.log({ tab });
       const resp = await chrome.tabs.sendMessage(tab.id, {
         isOffensive,
         reasons,
         multiClass,
       });
-      // do something with response here, not outside the function
-      console.log({resp});
+      console.log({ resp });
     }, 3000);
+  }
+});
+
+function showPopUp(data) {
+  console.log(data.details);
+  const reasons = data.details.offensive_reasons;
+  let reasonsList = "";
+  if (reasons.length > 0) {
+    reasonsList = "<ul>";
+    reasons.forEach((reason) => {
+      reasonsList += `<li>${reason}</li>`;
+    });
+    reasonsList += "</ul>";
+  }
+
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.scripting
+      .executeScript({
+        target: { tabId: tabs[0].id, allFrames: true },
+        func: (reasonsList, data) =>
+          document.body.insertAdjacentHTML(
+            "afterbegin",
+            `<div id="alert-offensive" style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(255, 255, 255, 0.9); z-index: 9999;">
+                    <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: #3498db; color:white; padding: 20px; box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2); border-radius: 10px; width: 700px;">
+                      <h2 style="margin-bottom: 10px;font-size:2rem; font-weight: bold;">Offensive Content Found on This Page!</h2>
+                      <p style="font-weight: 600; margin-top: 2rem;">Reasons:</p>
+                      ${reasonsList}
+                      <p style="font-weight: 600; margin-top: 2rem;">This content may be offensive due to its association with the following:</p>
+                      <span>${data.details.multi_class_result}</span>
+
+                      <p style="font-weight: 600; margin-top: 2rem; font-size: 1.5rem">You can close this tab</p>
+
+                      <div style="display: flex; margin-top: 2rem; align-items:center;">
+                      OR <button style="background-color: transparent; padding: 10px 15px; color: white; border-radius: 15px; text-decoration: underline;" onclick="document.getElementById('alert-offensive')?.remove()">Continue Anyway</button>
+                      </div>
+
+
+                  </div>`
+          ),
+        args: [reasonsList, data],
+      })
+      .then((res) => console.log("injected a function", res))
+      .catch((err) => console.log(err));
+  });
+}
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "sendTextToAPI") {
+    const formData = new FormData();
+    formData.append("user_input", message.text);
+    fetch("https://detection-main-production.up.railway.app/detect", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        if (!response.ok) {
+          console.error(
+            "Failed to send text to API",
+            response?.status,
+            response?.statusText
+          );
+          throw new Error("Failed to send text to API");
+        }
+        return response.json();
+      })
+      .then(async (data) => {
+        if (data?.details?.offensive) {
+          showPopUp(data);
+        } else {
+          console.log(`Text from ${message.url} is not offensive!`);
+        }
+      })
+      .catch((error) => {
+        console.log("Error sending text to API:", error);
+      });
   }
 });
