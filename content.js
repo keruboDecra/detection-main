cb7a35623a4097b0a60f5e60166892c1c3b2fead
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2024-03-18 09:36:35 +0200

    Update content.js

--- content.js
+++ content.js
@@ -1,3 +1,27 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   return false;
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Content script loaded");
+  const pageText = document.body.innerText.trim();
+  chrome.runtime.sendMessage({ action: "sendTextToAPI", text: pageText });
+});
+
+function getText() {
+  return document.body.innerText;
+}
+function getHTML() {
+  return document.body.outerHTML;
+}
+function getURL() {
+  return window.location.href;
+}
+
+// send url, text, and html to background.js
+chrome.runtime.sendMessage({
+  action: "sendTextToAPI",
+  text: getText(),
+  html: getHTML(),
+  url: getURL(),
+});
